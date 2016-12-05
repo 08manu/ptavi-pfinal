eb7bc075f02fb09ffec175bbb87c0610aec86d23
Manuel Antonio Viso Troyano <maviso@gamma30.aulas.gsyc.urjc.es>
2016-12-05 18:43:42 +0100

    Completando el uaclient

--- uaclient.py
+++ uaclient.py
@@ -6,32 +6,54 @@ import sys
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
+config = sys.argv[1]
+metodo = sys.argv[2]
+opcion = sys.argv[3]
 
 class uaCLIENT(ContentHandler):
 
     def __init__ (self):
 
         self.Lista = []
-        self.Diccionario = {'account': ['username', 'passwd'], 'uaserver': ['ip', 'puerto'], 'rtpaudio': ['puerto'], 'regproxy':['ip', 'puertp'], 'log':['path'], 'audio':['path']}
+        self.Diccionario = {'account': ['username', 'passwd'],
+                            'uaserver': ['ip', 'puerto'],
+                            'rtpaudio': ['puerto'], 'regproxy':['ip', 'puerto'],
+                            'log':['path'], 'audio':['path']}
 
-    if len(sys.argv) != 4:
-        sys.exit("Usage: python uaclient.py config method option")
+#    if len(sys.argv) != 4:
+#        sys.exit("Usage: python uaclient.py config method option")
 
-    def startElement(self, etiqueta, atrib)
-        Dicc = {}
-        etiqueta in self.Diccionario:
+    def startElement(self, etiqueta, atrib):
+        if etiqueta in self.Diccionario:
+            Dicc = {}
+            for atributo in self.Diccionario[etiqueta]:
+                Dicc[atributo] = atrib.get(atributo, "")
+            self.Lista.append([etiqueta, Dicc])
+
+    def get_tags(self):
+        return self.Lista
+
+
 
 if __name__ == "__main__":
 
+    parser = make_parser()
+    MyHandler = uaCLIENT()
+    parser.setContentHandler(MyHandler)
+    parser.parse(open(config))
+    Lista_xml = MyHandler.get_tags()
+    account_us = Lista_xml[0][1]['username']
+    account_passwd = Lista_xml[0][1]['passwd']
+    uaserver_ip = Lista_xml[1][1]['ip']
+    uaserver_puerto = Lista_xml[1][1]['puerto']
+    rtpaudio_puerto = Lista_xml[2][1]['puerto']
+    regproxy_ip = Lista_xml[3][1]['ip']
+    regproxy_puerto = Lista_xml[3][1]['puerto']
+    log_path = Lista_xml[4][1]['path']
+    audio_path = Lista_xml[5][1]['path']
 
-    fichero = open(sys.argv[1], "r")
-    for archivo in fichero.readlines()
-        print(archivo)
-    config = sys.argv[2]
-    metodo = sys.argv[3]
-    opcional = sys.argv[4]
 
     my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    my_socket.connect((config))
+    my_socket.connect((uaserver_ip, int(uaserver_puerto)))
 
